652029f235bbbcb7b44db9b05f6b993582a0743b
thai-ru <robinthairu34@gmail.com>
2024-06-01 10:09:18 +0300

    retrofit interface to fetch photos

---
+++ app/src/main/java/com/robin/marsphotos/network/MarsApiService.kt
@@ -0,0 +1,17 @@
+package com.robin.marsphotos.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+
+
+private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+interface MarsApiService {
+    @GET("photos")
+    fun getPhotos(): String
+}
